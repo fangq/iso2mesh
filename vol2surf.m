96b9c0813f046bb2f835c6cfd3246f4ff95dd98e
Qianqian Fang <fangqq@gmail.com>
2008-10-28 19:43:43 +0000

    split vol2mesh to vol2surf and surf2mesh
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@33 786e58fb-9377-0410-9ff7-e4ac0ac0635c

---
+++ vol2surf.m
@@ -0,0 +1,52 @@
+function [no,el]=vol2surf(img,ix,iy,iz,opt,dofix)
+
+img=img(ix,iy,iz);
+dim=size(img);
+newdim=dim+[2 2 2];
+newimg=zeros(newdim);
+newimg(2:end-1,2:end-1,2:end-1)=img;
+
+exesuff='.exe';
+if(isunix) exesuff=['.',mexext]; end
+
+maxlevel=max(newimg(:));
+el=[];
+no=[];
+for i=0:maxlevel-1
+  fprintf(1,'processing threshold level %d...\n',i);
+
+  [f0,v0]=isosurface(newimg,i);
+  v0(:,[1 2])=v0(:,[2 1]); % isosurface(V,th) assumes x/y transposed
+  if(dofix)  [v0,f0]=meshcheckrepair(v0,f0);  end
+  
+  if(isstruct(opt) & length(opt)==maxlevel) keepratio=opt(i+1).keepratio;
+  elseif (isstruct(opt) & length(opt)==1) keepratio=opt.keepratio;
+  else keepratio=opt;  end;
+
+  % first, resample the surface mesh with cgal
+  fprintf(1,'resampling surface mesh for level %d...\n',i);
+  [v0,f0]=meshresample(v0,f0,keepratio);
+  if(dofix) [v0,f0]=meshcheckrepair(v0,f0); end
+  
+  % if a transformation matrix/offset vector supplied, apply them
+  if(isstruct(opt) & length(opt)==maxlevel & isfield(opt(i+1),'A') & isfield(opt(i+1),'B')) 
+      v0=(opt(i+1).A*v0'+repmat(opt(i+1).B(:),1,size(v0,1)))';
+  elseif (isstruct(opt) & length(opt)==1 & isfield(opt,'A') & isfield(opt,'B')) 
+      v0=(opt.A*v0'+repmat(opt.B(:),1,size(v0,1)))';
+  end
+
+  if(i==0)
+      el=f0;
+      no=v0;
+  else
+      el=[el;f0+length(no)];
+      no=[no;v0];
+  end
+end
+
+if(isstruct(opt) & isfield(opt,'surf'))
+   for i=1:length(opt.surf)
+        el=[el;opt.surf(i).elem+length(no)];
+        no=[no;opt.surf(i).node];
+   end
+end
