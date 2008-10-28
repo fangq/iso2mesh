d31e8adff107ec2e91ac8cee64b122c4079f8072
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2008-10-28 19:44:17 +0000

    other cosmatic changes

--- trunk/iso2mesh/deislands3d.m
+++ trunk/iso2mesh/deislands3d.m
@@ -4,15 +4,15 @@ maxisland=-1;
 if(nargin==2) maxisland=sizelim; end
 
 for i=1:size(img,1)
-    fprintf(1,'scan x=%d\n',i);
+    if(mod(i,10)==0) fprintf(1,'processing slice x=%d\n',i); end
     img(i,:,:)=deislands2d(img(i,:,:),maxisland);
 end
 for i=1:size(img,2)
-    fprintf(1,'scan y=%d\n',i);
+    if(mod(i,10)==0) fprintf(1,'processing slice y=%d\n',i); end
     img(:,i,:)=deislands2d(img(:,i,:),maxisland);
 end
 for i=1:size(img,3)
-    fprintf(1,'scan z=%d\n',i);
+    if(mod(i,10)==0) fprintf(1,'processing slice z=%d\n',i); end
     img(:,:,i)=deislands2d(img(:,:,i),maxisland);
 end
 
