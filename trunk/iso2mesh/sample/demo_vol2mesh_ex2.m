f4a5d2661bda26dec6273c7204af10b53066ad65
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2008-10-28 19:41:33 +0000

    rename scripts, use tif to reduce sizes, add new examples

---
+++ trunk/iso2mesh/sample/demo_vol2mesh_ex2.m
@@ -0,0 +1,21 @@
+% sample script to create volumetric mesh from 
+% multiple levelsets of a binary segmented head image.
+%
+% Author: Qianqian Fang <fangq at nmr.mgh.harvard.edu>
+
+% load iso2mesh image
+
+img=imread('iso2mesh_bar.tif');
+img=fliplr(img);
+fullimg=repmat(1-img,[1,1,30]);
+fullimg(:,:,31:60)=repmat(ones(size(img)),[1,1,30]);
+
+% create volumetric tetrahedral mesh from the two-layer 3D images
+% this may take another few minutes for a 256x256x256 volume
+opt.keepratio=0.1;
+tic
+[node,elem,bound]=vol2mesh(fullimg,1:size(fullimg,1),1:size(fullimg,2),1:size(fullimg,3),opt,100,1);
+toc
+
+hb=trisurf(bound(:,1:3),node(:,2),node(:,1),node(:,3));
+axis equal
