c4579efda08cc9106dc9cfdc876a6c3fe44a2691
Qianqian Fang <fangqq@gmail.com>
2008-11-02 19:01:02 +0000

    final polishing of the documents
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@40 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- mcpath.m
+++ mcpath.m
@@ -1,16 +1,17 @@
 function binname=mcpath(fname)
 % tempname=mcpath(fname)
-% return a command file name by prepending meshing command directory path
+% get full executable path by prepending a command directory path
 % parameters:
 %    fname: input, a file name string
 %    tempname: output, full file name located in the bin directory
+%
+%    if global variable ISO2MESH_BIN is set in 'base', it will
+%    use [ISO2MESH_BIN filesep cmdname] as the command full path,
+%    otherwise, let matlab pass the cmdname to the shell, which
+%    will search command in the directories listed in system
+%    $PATH variable.
 
-userpath=evalin('base','exist(''ISO2MESH_BIN'')');
-if(userpath==1) 
-	p=evalin('base','ISO2MESH_BIN'); 
-else
-	p=[];
-end
+p=getvarfrom('base','ISO2MESH_BIN');
 tempname=[];
 if(isempty(p) | ~exist(p))
 	binname=fname;
