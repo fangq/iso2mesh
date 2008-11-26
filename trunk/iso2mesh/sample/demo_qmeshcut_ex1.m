ee91d72724f6fdd0b9d3da75840d4774588bbb4c
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2008-11-26 20:03:08 +0000

    minor changes for qmeshcut example

--- trunk/iso2mesh/sample/demo_qmeshcut_ex1.m
+++ trunk/iso2mesh/sample/demo_qmeshcut_ex1.m
@@ -1,3 +1,7 @@
+% qmeshcut demonstration
+%
+% by Qianqian Fang, <fangq at nmr.mgh.harvard.edu>
+%
 % to demonstrate how to use qmeshcut to produce cross-sectional plot 
 % of an un-structured (tetrahedral) mesh
 
@@ -5,11 +9,15 @@
 
 demo_vol2mesh_ex1
 
-% define a plane by 3 points, in this case, z=15
+% define a plane by 3 points, in this case, z=mean(node(:,3))
 
-plane=[0 0 15; 0 20 15; 20 0 15];
+z0=mean(node(:,3));
 
-% run qmeshcut to get the cross-section information at z=15
+plane=[min(node(:,1)) min(node(:,2)) z0
+       min(node(:,1)) max(node(:,2)) z0
+       max(node(:,1)) min(node(:,2)) z0];
+
+% run qmeshcut to get the cross-section information at z=mean(node(:,1))
 % use the x-coordinates as the nodal values
 
 [cutpos,cutvalue,facedata]=qmeshcut(elem,node,node(:,1),plane);
@@ -17,7 +25,8 @@ plane=[0 0 15; 0 20 15; 20 0 15];
 % plot your results
 
 figure;
-trimesh(bound(:,1:3),node(:,1),node(:,2),node(:,3),'facecolor','none');
+hsurf=trimesh(bound(:,1:3),node(:,1),node(:,2),node(:,3),'facecolor','none');
 hold on;
-patch('Vertices',cutpos,'Faces',facedata,'FaceVertexCData',cutvalue,'facecolor','interp');
+hcut=patch('Vertices',cutpos,'Faces',facedata,'FaceVertexCData',cutvalue,'facecolor','interp');
+%set(hcut, 'linestyle','none')
 axis equal;
