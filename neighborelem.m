82fc7fe0fe0cfe45c98c45bb54e136aec2590e8e
Qianqian Fang <fangqq@gmail.com>
2008-12-12 19:08:52 +0000

    add neighborelem and getplanefrom3pt
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@76 786e58fb-9377-0410-9ff7-e4ac0ac0635c

---
+++ neighborelem.m
@@ -0,0 +1,28 @@
+function [conn,connnum,count]=neighborelem(elem,nn);
+% [conn,connnum,count]=neighborelem(elem,nn)
+% neighborelem: create node neighbor list from a mesh
+% author: fangq (fangq<at> nmr.mgh.harvard.edu)
+% date: 2007/11/21
+%
+% parameters:
+%    elem:  element table of a mesh
+%    nn  :  total node number of the mesh
+%    conn:  output, a cell structure of length nn, conn{n}
+%           contains a list of all neighboring elem ID for node n
+%    connnum: vector of length nn, denotes the neighbor number of each node
+%    count: total neighbor numbers
+
+conn=cell(nn,1);
+dim=size(elem);
+for i=1:dim(1)
+  for j=1:dim(2)
+    conn{elem(i,j)}=[conn{elem(i,j)},i];
+  end
+end
+count=0;
+connnum=zeros(1,nn);
+for i=1:nn
+    conn{i}=sort(conn{i});
+    connnum(i)=length(conn{i});
+    count=count+connnum(i);
+end
