2e81fe8c179224ffeb597612f064df9abc35c38b
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2008-12-18 17:50:53 +0000

    provide instructions when user encounters permission deny error

--- trunk/iso2mesh/saveoff.m
+++ trunk/iso2mesh/saveoff.m
@@ -8,9 +8,18 @@ function saveoff(v,f,fname)
 %      f: input, surface face element list, dimension (be,3)
 %      fname: output file name
 
-fid=fopen(fname,'wt');
-fprintf(fid,'OFF\n');
-fprintf(fid,'%d %d %d\n',length(v),length(f),0);
-fprintf(fid,'%f %f %f\n',v');
-fprintf(fid,'3 %d %d %d\n',(f-1)');
-fclose(fid);
+try
+	fid=fopen(fname,'wt');
+	fprintf(fid,'OFF\n');
+	fprintf(fid,'%d %d %d\n',length(v),length(f),0);
+	fprintf(fid,'%f %f %f\n',v');
+	fprintf(fid,'3 %d %d %d\n',(f-1)');
+	fclose(fid);
+catch
+    error(['You do not have permission to delete temporary files, if you are working in a multi-user ',...
+         'environment, such as Unix/Linux and there are other users using iso2mesh, ',...
+         'you may need to define ISO2MESH_SESSION=''yourstring'' to make your output ',...
+         'files different from others; if you do not have permission to ',mwpath(''),...
+         ' as the temporary directory, you have to define ISO2MESH_TEMP=''/path/you/have/write/permission'' ',...
+         'in matlab/octave base workspace.']);
+end
