32e3f13ffc82612277e067420893994c21dadaa3
Qianqian Fang <fangqq@gmail.com>
2009-02-08 01:49:25 +0000

    move the node correction from vol2mesh to vol2surf
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@105 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- vol2mesh.m
+++ vol2mesh.m
@@ -1,19 +1,19 @@
 function [node,elem,bound]=vol2mesh(img,ix,iy,iz,opt,maxvol,dofix,method)
+%   [node,elem,bound]=vol2mesh(img,ix,iy,iz,opt,maxvol,dofix,method)
 %   convert a binary (or multi-valued) volume to tetrahedral mesh
+%
 %   author: Qianqian Fang (fangq <at> nmr.mgh.harvard.edu)
 %   inputs:
 %          img: a volumetric binary image
 %          ix,iy,iz: subvolume selection indices in x,y,z directions
-%          opt: additional parameters
-%            opt=a float less than 1: compression rate for surf. simplification
-%            opt.keeyratio=a float less than 1: same as above, same for all surf.
-%            opt(1,2,..).keeyratio: setting compression rate for each levelset
-%            opt(1,2,..).surf.{node,elem}: add additional surfaces
-%            opt(1,2,..).{A,B}: linear transformation for each surface
+%          opt: as defined in vol2surf.m
 %          maxvol: target maximum tetrahedral elem volume
 %          dofix: 1: perform mesh validation&repair, 0: skip repairing
+%
+%   outputs:
 %          node: output, node coordinates of the tetrahedral mesh
-%          elem: output, element list of the tetrahedral mesh
+%          elem: output, element list of the tetrahedral mesh, the last 
+%               column is the region id
 %          bound: output, mesh surface element list of the tetrahedral mesh
 %               the last column denotes the boundary ID
 
@@ -25,12 +25,3 @@ else
 end
 %then, create volumetric mesh from the surface mesh
 [node,elem,bound]=surf2mesh(no,el,[],[],1,maxvol,regions,holes);
-
-
-%some final fix and scaling
-node=node-1; % because we padded the image with a 1 voxel thick null layer in newimg
-
-dim=(size(img));
-node(:,1)=node(:,1)*(max(ix)-min(ix)+1)/dim(1)+(min(ix)-1);
-node(:,2)=node(:,2)*(max(iy)-min(iy)+1)/dim(2)+(min(iy)-1);
-node(:,3)=node(:,3)*(max(iz)-min(iz)+1)/dim(3)+(min(iz)-1);
