1e98baf9b4d27004fbdb5593cae31cb48d047b4a
Qianqian Fang <fangqq@gmail.com>
2009-02-08 05:36:08 +0000

    script to sync documents
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@111 786e58fb-9377-0410-9ff7-e4ac0ac0635c

---
+++ doc/gendoc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# commands to update the document pages from homepage
+
+lynx -dump -width 100 "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Download&embed=1" > Download_and_License.txt
+lynx -dump "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Doc/Installation&embed=1" > INSTALL.txt
+lynx -dump "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Doc/Basics&embed=1" > Get_Started.txt
+
