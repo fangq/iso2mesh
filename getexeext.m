d7d06d3d4ebbf62b1e0907a5c99c0acbb5b43b22
Qianqian Fang <fangqq@gmail.com>
2009-02-09 21:51:25 +0000

    use universal binary for mac os
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@115 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- getexeext.m
+++ getexeext.m
@@ -3,6 +3,9 @@ function exesuff=getexeext()
 exesuff='.exe';
 if(isunix) 
 	exesuff=['.',mexext];
+	if(strcmp(exesuff,'.mexmaci'))
+		exesuff='.mexmac'; % will use universal binary for Mac
+	end
 end
 if(isoctavemesh)
       if(isempty(strfind(computer,'msdos')))
