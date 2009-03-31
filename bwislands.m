ffb69b84e587d14fb377e5bcbc09f2c1b0b4ed48
Qianqian Fang <fangqq@gmail.com>
2009-03-31 18:03:20 +0000

    updates to bwisland, mcpath now search bin folder first
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@134 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- bwislands.m
+++ bwislands.m
@@ -1,4 +1,16 @@
 function islands=bwislands(img)
+%   islands=bwislands(img)
+%
+%   return the indices of non-zero elements in a 2D or 3D image
+%   grouped by connected regions in a cell array
+%
+%   author: Qianqian Fang (fangq <at> nmr.mgh.harvard.edu)
+%   input:
+%          img: a 2D or 3D array
+%   output:
+%          islands: a cell array, each cell records the indices 
+%                   of the non-zero elements in img for a connected 
+%                   region (or an island)
 
 img=logical(1-img);
 idx=find(1-img(:));
@@ -6,7 +18,7 @@ islands={};
 
 count=1;
 while(length(idx))
-    [I,J]=ind2sub(size(img),idx(1));
+        [I,J]=ind2sub(size(img),idx(1));
 	imgnew=imfill(img,[I,J]);
 	islands{count}=find(imgnew~=img);
 	count=count+1;
