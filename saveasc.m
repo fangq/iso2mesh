f7394b335d8acb0968fffd42b91c9614f88060eb
Qianqian Fang <fangqq@gmail.com>
2009-04-02 15:47:45 +0000

    write to freesurfer asc file format
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@137 786e58fb-9377-0410-9ff7-e4ac0ac0635c

---
+++ saveasc.m
@@ -0,0 +1,25 @@
+function saveasc(v,f,fname)
+% saveasc(v,f,fname)
+% save a surface mesh to FreeSurfer ASC mesh format
+% author: Qianqian Fang (fangq<at> nmr.mgh.harvard.edu)
+% date: 2009/04/02
+%
+% parameters:
+%      v: input, surface node list, dimension (nn,3)
+%      f: input, surface face element list, dimension (be,3)
+%      fname: output file name
+
+fid=fopen(fname,'wt');
+if(fid==-1)
+    error(['You do not have permission to save mesh files, if you are working in a multi-user ',...
+         'environment, such as Unix/Linux and there are other users using iso2mesh, ',...
+         'you may need to define ISO2MESH_SESSION=''yourstring'' to make your output ',...
+         'files different from others; if you do not have permission to ',mwpath(''),...
+         ' as the temporary directory, you have to define ISO2MESH_TEMP=''/folder/you/have/write/permission'' ',...
+         'in matlab/octave base workspace.']);
+end
+fprintf(fid,'#!ascii raw data file %s\n',fname);
+fprintf(fid,'%d %d\n',length(v),length(f));
+fprintf(fid,'%f %f %f 0\n',v');
+fprintf(fid,'%d %d %d 0\n',(f-1)');
+fclose(fid);
