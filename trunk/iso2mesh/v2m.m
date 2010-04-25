0ffe6a6fd8164d3783a04e21c075d4bc2434bb29
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2010-04-25 21:37:16 +0000

    add meaningful help text for the shortcuts

--- trunk/iso2mesh/v2m.m
+++ trunk/iso2mesh/v2m.m
@@ -1,7 +1,8 @@
 function [node,elem,face]=v2m(img,isovalues,opt,maxvol,method)
 % [node,elem,face]=v2m(img,isovalues,opt,maxvol,method)
 %
-% short-hand version of vol2mesh
+% volumetric mesh generation from binary or gray-scale volumetric images
+% shortcut for vol2mesh
 %
 % author: Qianqian Fang (fangq <at> nmr.mgh.harvard.edu)
 %
