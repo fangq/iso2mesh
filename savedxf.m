3a7386b9a0e231f6bfbc5b9cd57ce74abc529c0c
Qianqian Fang <fangqq@gmail.com>
2010-04-25 21:02:31 +0000

    debug DXF exporter, works for Blender and SolidWorks eDrawing
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@185 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- savedxf.m
+++ savedxf.m
@@ -25,16 +25,26 @@ if(fid==-1)
          'in matlab/octave base workspace.']);
 end
 
-fprintf(fid,'0\nSECTION\n2\nHEADER\n0\nENDSEC\n0\nSECTION\n2\nBLOCKS\n0\nBLOCK\n');
+fprintf(fid,'0\nSECTION\n2\nHEADER\n0\nENDSEC\n0\nSECTION\n2\nENTITIES\n');
+
+if(~isempty(face))
+  fprintf(fid,'0\nPOLYLINE\n66\n1\n8\nI2M\n70\n64\n71\n%d\n72\n%d\n',size(node,1),size(face,1));
+end
 
 if(~isempty(node))
-  fprintf(fid,'0\nVERTEX\n8\nMeshes\n10\n%f\n20\n%f\n30\n%f\n70\n192\n',node');
+  fprintf(fid,'0\nVERTEX\n8\nI2M\n10\n%f\n20\n%f\n30\n%f\n70\n192\n',node');
 end
 
 if(~isempty(face))
-  fprintf(fid,'0\nVERTEX\n8\nMeshes\n62\n254\n10\n0.0\n20\n0.0\n30\n0.0\n70\n128\n71\n%d\n72\n%d\n73\n%d\n',(face-1)');
+  fprintf(fid,'0\nVERTEX\n8\nI2M\n62\n254\n10\n0.0\n20\n0.0\n30\n0.0\n70\n128\n71\n%d\n72\n%d\n73\n%d\n',face');
 end
 
-fprintf(fid,'0\nSEQEND\n0\nENDBLK\n0\nENDSEC\n0\nEOF\n');
+fprintf(fid,'0\nSEQEND\n0\nENDSEC\n');
+
+if(~isempty(face))
+  fprintf(fid,'0\nSECTION\n2\nENTITIES\n0\nINSERT\n8\n1\n2\nMesh\n10\n0.0\n20\n0.0\n30\n0.0\n41\n1.0\n42\n1.0\n43\n1.0\n50\n0.0\n0\nENDSEC\n');
+end
+
+fprintf(fid,'0\nEOF');
 
 fclose(fid);
