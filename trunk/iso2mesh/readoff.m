f1791c6821d6f415884d74b5c136725bc9169f18
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2010-05-05 17:08:50 +0000

    close file handle after reading

--- trunk/iso2mesh/readoff.m
+++ trunk/iso2mesh/readoff.m
@@ -22,6 +22,7 @@ line=fgetl(fid);
 dim=fscanf(fid,'%d',3);
 node=fscanf(fid,'%f',[3,dim(1)])';
 elem=fscanf(fid,'%f',inf);
+fclose(fid);
 if(length(elem)==4*dim(2))
     elem=reshape(elem,[4,dim(2)])';
 elseif(length(elem)==8*dim(2))
