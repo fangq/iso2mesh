b7f1da8cd64bfa3d419aeaf4056a5f62ee579f45
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2010-05-05 22:52:40 +0000

    add nodevolume, elemvolume from FEMcore and meshreorder

---
+++ trunk/iso2mesh/meshreorder.m
@@ -0,0 +1,26 @@
+function newelem=meshreorder(node,elem)
+% newelem=meshreorder(node,elem)
+%
+% reorder nodes in a surface or tetrahedral mesh to ensure all
+% elements are oriented consistently
+%
+% author: fangq (fangq<at> nmr.mgh.harvard.edu)
+% date: 2010/05/05
+%
+% input:
+%    node: list of nodes
+%    elem: list of elements (each row are indices of nodes of each element)
+%
+% output:
+%    newelem: the element list with consistent ordering
+%
+% -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
+%
+
+% calculate the canonical volume of the element (can be a 2D or 3D)
+vol=elemvolume(elem,node,'signed');
+
+% make sure all elements are positive in volume
+idx=find(vol<0);
+elem(idx,[end-1,end])=elem(idx,[end,end-1]);
+newelem=elem;
