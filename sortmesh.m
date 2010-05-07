125466d4f33cb5259599d607e6ea7adec4f73689
Qianqian Fang <fangqq@gmail.com>
2010-05-07 18:41:03 +0000

    specify columns to sort for sortmesh
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@197 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- sortmesh.m
+++ sortmesh.m
@@ -1,4 +1,4 @@
-function [no,el,fc]=sortmesh(origin,node,elem,face)
+function [no,el,fc,nodemap]=sortmesh(origin,node,elem,ecol,face,fcol)
 % [no,el,fc]=sortmesh(origin,node,elem,face)
 %
 % sort nodes and elements in a mesh so that the indexed
@@ -14,12 +14,15 @@ function [no,el,fc]=sortmesh(origin,node,elem,face)
 %            node(1,:)
 %    node: list of nodes
 %    elem: list of elements (each row are indices of nodes of each element)
+%    ecol: list of columns in elem to participate sorting
 %    face: list of surface triangles (this can be omitted)
+%    fcol: list of columns in face to participate sorting
 %
 % output:
 %    no: node coordinates in the sorted order
 %    el: the element list in the sorted order
 %    fc: the surface triangle list in the sorted order (can be ignored)
+%    nodemap: the new node mapping order, no=node(nodemap,:)
 %
 % -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
 %
@@ -30,14 +33,22 @@ end
 sdist=node-repmat(origin,size(node,1),1);
 [theta,phi,R]=cart2sph(sdist(:,1),sdist(:,2),sdist(:,3));
 sdist=[R,phi,theta];
-[nval,nidx]=sortrows(sdist);
-no=node(nidx,:);
+[nval,nodemap]=sortrows(sdist);
+no=node(nodemap,:);
 
-[nval,nidx]=sortrows(nidx);
-el=sort(nidx(elem),2);
-el=sortrows(el);
+[nval,nidx]=sortrows(nodemap);
+el=elem;
+if(nargin<4)
+   ecol=1:size(elem,2);
+end
+el(:,ecol)=sort(nidx(elem(:,ecol)),2);
+el=sortrows(el,ecol);
 
-if(nargin>=4 && nargout==3)
-  fc=sort(nidx(face),2);
-  fc=sortrows(fc);
+if(nargin>=5 && nargout==3)
+  if(nargin<6)
+     fcol=1:size(face,2);
+  end
+  fc=face;
+  fc(:,fcol)=sort(nidx(face(:,fcol)),2);
+  fc=sortrows(fc,fcol);
 end
