67b77e45381d003cd8adfafaa6c790419d1ba23c
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2010-06-06 14:55:14 +0000

    add thining volume

---
+++ trunk/iso2mesh/thinbinvol.m
@@ -0,0 +1,27 @@
+function vol=thinbinvol(vol,layer)
+% vol=thickenbinvol(vol,layer)
+%
+% this is similar to bwmorph(vol,'thicken',layer) except this does it
+% in 3d and only does thickening for non-zero elements (and hopefully faster)
+%
+% author: fangq (fangq<at> nmr.mgh.harvard.edu)
+%
+% input:
+%     vol: a volumetric binary image
+%     layer: number of iterations for the thickenining
+% output:
+%     vol: the volume image after the thickening
+%
+% -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
+%
+
+dim=size(vol);
+dxy=dim(1)*dim(2);
+fulllen=prod(dim);
+
+for i=1:layer
+	idx=find(~vol);
+	idxnew=[idx+1; idx-1;idx+dim(1);idx-dim(1);idx+dxy;idx-dxy];
+	idxnew=idxnew(find(idxnew>0 & idxnew<fulllen));
+	vol(idxnew)=0;
+end
