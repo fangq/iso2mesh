a96717729cfa513b1898e5767eaa5ea7a9b0b5b3
Qianqian Fang <fangqq@gmail.com>
2010-07-14 04:51:59 +0000

    update documentation for the coming release of 1.0beta
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@225 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- doc/gendoc.sh
+++ doc/gendoc.sh
@@ -2,9 +2,9 @@
 
 # commands to update the document pages from homepage
 
-lynx -dump -width 100 "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Download&embed=1" > Download_and_License.txt
+lynx -dump -width 150 "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Download&embed=1" > Download_and_License.txt
 lynx -dump "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Doc/Installation&embed=1" > INSTALL.txt
 lynx -dump "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Doc/Basics&embed=1" > Get_Started.txt
 lynx -dump "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Doc/FAQ&embed=1" > FAQ.txt
 
-wget http://iso2mesh.sourceforge.net/upload/iso2mesh_workflow_v08.png -Oiso2mesh_workflow.png
+wget http://iso2mesh.sourceforge.net/upload/iso2mesh_workflow_v09.jpg -Oiso2mesh_workflow.jpg
