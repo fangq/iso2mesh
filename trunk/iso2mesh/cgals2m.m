a65486e7e9eb62163d746b09c303d5c8ebc4fade
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2010-12-22 22:54:14 +0000

    RNG seeding support in cgalpoly, update documents

--- trunk/iso2mesh/cgals2m.m
+++ trunk/iso2mesh/cgals2m.m
@@ -55,13 +55,20 @@ end
 
 saveoff(v,f,mwpath('pre_cgalpoly.off'));
 deletemeshfile(mwpath('post_cgalmesh.mesh'));
-cmd=sprintf('"%s%s" "%s" "%s" %f %f %f %f %f',mcpath('cgalpoly'),exesuff,...
+
+randseed=hex2dec('623F9A9E'); % "U+623F U+9A9E"
+
+if(~isempty(getvarfrom('base','ISO2MESH_RANDSEED')))
+        randseed=getvarfrom('base','ISO2MESH_RANDSEED');
+end
+
+cmd=sprintf('"%s%s" "%s" "%s" %f %f %f %f %f %d',mcpath('cgalpoly'),exesuff,...
     mwpath('pre_cgalpoly.off'),mwpath('post_cgalpoly.mesh'),ang,ssize,...
-    approx,reratio,maxvol);
+    approx,reratio,maxvol,randseed);
 system(cmd);
 if(~exist(mwpath('post_cgalpoly.mesh'),'file'))
     error(['output file was not found, something must have gone wrong when running command: \n',cmd]);
 end
-[node,elem,face]=readmedit(mwpath('post_cgalmesh.mesh'));
+[node,elem,face]=readmedit(mwpath('post_cgalpoly.mesh'));
 
 fprintf(1,'surface and volume meshes complete\n');
--- trunk/iso2mesh/doc/gendoc.sh
+++ trunk/iso2mesh/doc/gendoc.sh
@@ -6,5 +6,6 @@ lynx -dump -width 150 "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keyword
 lynx -dump "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Doc/Installation&embed=1" > INSTALL.txt
 lynx -dump "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Doc/Basics&embed=1" > Get_Started.txt
 lynx -dump "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Doc/FAQ&embed=1" > FAQ.txt
+lynx -dump "http://iso2mesh.sourceforge.net/cgi-bin/index.cgi?keywords=Advanced&embed=1" > Advanced_Features.txt
 
 wget http://iso2mesh.sourceforge.net/upload/iso2mesh_workflow_v09.jpg -Oiso2mesh_workflow.jpg
