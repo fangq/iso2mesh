6bb0dee82f51bf01be38e1c7465c4af4d49d59ca
Qianqian Fang <fangqq@gmail.com>
2011-11-18 15:24:40 +0000

    let s2m support cgalpoly
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@324 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- s2m.m
+++ s2m.m
@@ -1,4 +1,4 @@
-function [node,elem,face]=s2m(v,f,keepratio,maxvol)
+function [node,elem,face]=s2m(v,f,keepratio,maxvol,method)
 %
 % [node,elem,face]=s2m(v,f,keepratio,maxvol)
 %
@@ -8,10 +8,19 @@ function [node,elem,face]=s2m(v,f,keepratio,maxvol)
 %
 % inputs and outputs are similar to those defined in surf2mesh
 %
+% if method='cgalpoly', s2m will call cgals2m and keepratio should be a 
+% structure (as the 'opt' input in cgals2m)
+%
 % -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
 %
 
 p0=min(v(:,1:3));
 p1=max(v(:,1:3));
+if(nargin>=5)
+  if(strcmp(method,'cgalpoly'))
+    [node,elem,face]=cgals2m(v,f,keepratio,maxvol);
+    return;
+  end
+end
 [node,elem,face]=surf2mesh(v,f,p0,p1,keepratio,maxvol,[],[]);
 
