9f036aeb086e9089f8811c7957d4d633c8166e31
Qianqian Fang <fangqq@gmail.com>
2012-02-14 22:09:45 +0000

    use trisurf in octave if available
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@346 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- plotsurf.m
+++ plotsurf.m
@@ -113,14 +113,15 @@ rand ('state',rngstate);
 
 %-------------------------------------------------------------------------
 function hh=plotasurf(node,face,varargin)
+isoct=isoctavemesh;
 if(size(node,2)==4)
-	if(isoctavemesh)
-	    h=trisurf(face(:,1:3),node(:,1),node(:,2),node(:,3),node(:,4),'edgecolor','k',varargin{:});
+	if(isoct && ~exist('trisurf','file'))
+	    h=trimesh(face(:,1:3),node(:,1),node(:,2),node(:,3),node(:,4),'edgecolor','k',varargin{:});
 	else
 	    h=trisurf(face(:,1:3),node(:,1),node(:,2),node(:,3),node(:,4),varargin{:});
 	end
 else
-	if(isoctavemesh)
+	if(isoct && ~exist('trisurf','file'))
 	    h=trimesh(face(:,1:3),node(:,1),node(:,2),node(:,3),'edgecolor','k',varargin{:});
 	else
 	    h=trisurf(face(:,1:3),node(:,1),node(:,2),node(:,3),varargin{:});
