361ce6f82eadce04a202d2ebe671df615de8c730
Qianqian Fang <fangqq@gmail.com>
2012-02-29 21:14:47 +0000

    add 'first' and 'second' options in surfboolean, add demo script for surfboolean and surface repairing
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/iso2mesh@362 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- surfboolean.m
+++ surfboolean.m
@@ -23,6 +23,8 @@ function [newnode,newelem,newelem0]=surfboolean(node,elem,varargin)
 %                  you can use newelem(find(mod(newelem(:,4),2)==1),:) to
 %                  get mesh 1 cut by mesh 2, or newelem(find(mod(newelem(:,4),2)==0),:) 
 %                  to get mesh 2 cut by mesh 1;
+%           'first': combine 1 and 3 from the output of 'all'
+%           'second': combine 2 and 4 from the output of 'all'
 %           'self': test for self-intersections; only the first mesh is
 %                   tested; other inputs are ignored.
 %
@@ -60,7 +62,7 @@ for i=1:3:len
    el=varargin{i+2};
    deletemeshfile(mwpath('pre_surfbool*.gts'));
    deletemeshfile(mwpath('post_surfbool.gts'));
-   if(strcmp(op,'all'))
+   if(strcmp(op,'all') || strcmp(op,'first') || strcmp(op,'second'))
       deletemeshfile(mwpath('s1out2.gts'));
       deletemeshfile(mwpath('s1in2.gts'));
       deletemeshfile(mwpath('s2out1.gts'));
@@ -68,23 +70,27 @@ for i=1:3:len
    end
    savegts(newnode(:,1:3),newelem(:,1:3),mwpath('pre_surfbool1.gts'));
    savegts(no(:,1:3),el(:,1:3),mwpath('pre_surfbool2.gts'));
+   opstr=op;
    if(strcmp(op,'self'))
-       op='inter -s';
+       opstr='inter -s';
+   end
+   if(strcmp(op,'first') || strcmp(op,'second'))
+       opstr='all';
    end
    cmd=sprintf('cd "%s";"%s%s" %s "%s" "%s" -v > "%s"',mwpath(''),mcpath('gtsset'),exesuff,...
-       op,mwpath('pre_surfbool1.gts'),mwpath('pre_surfbool2.gts'),mwpath('post_surfbool.gts'));
+       opstr,mwpath('pre_surfbool1.gts'),mwpath('pre_surfbool2.gts'),mwpath('post_surfbool.gts'));
    [status outstr]=system(cmd);
-   if(status~=0 && strcmp(op,'inter -s')==0)
+   if(status~=0 && strcmp(op,'self')==0)
        error(['surface boolean command failed:' cmd]);
    end
-   if(status~=0 && strcmp(op,'inter -s') && ~isempty(strfind(outstr,'(new_ear): assertion failed')))
+   if(status~=0 && strcmp(op,'self') && ~isempty(strfind(outstr,'(new_ear): assertion failed')))
        fprintf(1,'no self-intersection was found! (ignore the above error)\n');
        newnode=[];
        newelem=[];
        newelem0=[];
        return;
    end
-   if(strcmp(op,'all'))
+   if(strcmp(op,'all') || strcmp(op,'first') || strcmp(op,'second'))
       % tag the 4 piceses of meshes, this tag do not propagate to the next boolean operation
       [nnode nelem]=readgts(mwpath('s1out2.gts'));
       newelem=[nelem ones(size(nelem,1),1)];
@@ -101,9 +107,19 @@ for i=1:3:len
       [nnode nelem]=readgts(mwpath('s2in1.gts'));
       newelem=[newelem; nelem+size(newnode,1) 4*ones(size(nelem,1),1)];
       newnode=[newnode; nnode 4*ones(size(nnode,1),1)];
+
+      if(strcmp(op,'first'))
+          newelem=newelem(find(mod(newelem(:,4),2)==1),:);
+          [newnode,nelem]=removeisolatednode(newnode,newelem(:,1:3));
+          newelem=[nelem newelem(:,4)];
+      elseif(strcmp(op,'second'))
+          newelem=newelem(find(mod(newelem(:,4),2)==0),:);
+          [newnode,nelem]=removeisolatednode(newnode,newelem(:,1:3));
+          newelem=[nelem,newelem(:,4)];
+      end
    else
       [newnode,newelem]=readgts(mwpath('post_surfbool.gts'));
-      if(strcmp(op,'inter -s'))
+      if(strcmp(op,'self'))
           fprintf(1,'a total of %d self-intersecting elements were found\n',size(newelem,1));
           if(nargout>=3)
               [found,newelem0]=ismember(newnode,node,'rows');
