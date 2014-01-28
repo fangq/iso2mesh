999169d5564cf785f14e8a587797f234639de657
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2014-01-28 21:40:05 +0000

    import genT5mesh and genT6mesh scripts from mmc

--- trunk/iso2mesh/gendocs.sh
+++ trunk/iso2mesh/gendocs.sh
@@ -25,7 +25,8 @@ print_group()
 func_shortcut="v2m v2s s2m s2v sms"
 func_mainfun="vol2mesh vol2surf surf2mesh surf2vol"
 func_backend="binsurface cgalv2m cgals2m vol2restrictedtri surf2volz"
-func_primitive="meshabox meshasphere meshanellip meshunitsphere meshacylinder"
+func_primitive="meshabox meshasphere meshanellip meshunitsphere meshacylinder 
+                meshgrid5 meshgrid6"
 func_inquery="finddisconnsurf surfedge volface extractloops meshconn  
                 meshcentroid nodevolume elemvolume neighborelem 
 		faceneighbors edgeneighbors maxsurf flatsegment orderloopedge  
