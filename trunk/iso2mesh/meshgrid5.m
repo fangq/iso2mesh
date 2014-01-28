fd03b6f6c64709c03c84a7c9b2f249f660d8b8cf
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2014-01-28 21:42:11 +0000

    update meshgrid docs

--- trunk/iso2mesh/meshgrid5.m
+++ trunk/iso2mesh/meshgrid5.m
@@ -6,7 +6,7 @@ function [node,elem]=meshgrid5(varargin)
 % each hypercube into 5 tetrahedra
 %
 % author: John D'Errico
-% URL: http://www.mathkb.com/Uwe/Forum.aspx/matlab/50484/Constant-surfaces
+% URL: http://www.mathworks.com/matlabcentral/newsreader/view_thread/107191
 % modified by Qianqian Fang (fangq<at> nmr.mgh.harvard.edu)
 %
 % input:
