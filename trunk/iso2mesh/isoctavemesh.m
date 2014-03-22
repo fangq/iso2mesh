949d092e8d49355efd05a0cf7f91840dc8ee8ee1
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2014-03-22 21:58:26 +0000

    fix octave testing bug

--- trunk/iso2mesh/isoctavemesh.m
+++ trunk/iso2mesh/isoctavemesh.m
@@ -13,7 +13,7 @@ function [isoctave verinfo]=isoctavemesh
 % -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
 %
 verinfo='';
-isoctave=(exist('OCTAVE_VERSION')~=0);
+isoctave=(exist('OCTAVE_VERSION','builtin')~=0);
 if(nargout==2 && isoctave)
     verinfo=OCTAVE_VERSION;
 end
