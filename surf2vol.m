b63beb618f6827feb3cf395c2c0ae316097d33d5
Qianqian Fang <fangqq@gmail.com>
2016-04-02 00:18:15 -0400

    output world to voxel coordinate transformation

--- surf2vol.m
+++ surf2vol.m
@@ -1,6 +1,6 @@
-function img=surf2vol(node,face,xi,yi,zi)
+function [img, v2smap]=surf2vol(node,face,xi,yi,zi)
 %
-% img=surf2vol(node,face,xi,yi,zi)
+% [img, v2smap]=surf2vol(node,face,xi,yi,zi)
 %
 % convert a triangular surface to a shell of voxels in a 3D image
 %
@@ -13,6 +13,8 @@ function img=surf2vol(node,face,xi,yi,zi)
 %
 % output:
 %	 img: a volumetric binary image at position of ndgrid(xi,yi,zi)
+%        v2smap (optional): a 4x4 matrix denoting the Affine transformation to map
+%             the voxel coordinates back to the mesh space.
 %
 % -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
 %
@@ -23,3 +25,16 @@ img=surf2volz(node(:,1:3),face(:,1:3),xi,yi,zi);
 img=img | shiftdim(surf2volz(node(:,[3 1 2]),face(:,1:3),zi,xi,yi),1);
 img=img | shiftdim(surf2volz(node(:,[2 3 1]),face(:,1:3),yi,zi,xi),2);
 
+v2smap=[];
+
+% here we assume the grid is uniform; surf2vol can handle non-uniform grid, 
+% but the affine output is not correct in this case
+
+if(nargout>1) 
+    dlen=abs([xi(2)-xi(1) yi(2)-yi(1) zi(2)-zi(1)]);
+    p0=min(node);
+    offset=p0;
+    v2smap=diag(abs(dlen));
+    v2smap(4,4)=1;
+    v2smap(1:3,4)=offset';
+end
