059f98aa96612f196156542d3deea8350dc0a1e8
Qianqian Fang <fangqq@gmail.com>
2017-06-20 17:56:35 -0400

    support initial angle in orthdisk

--- orthdisk.m
+++ orthdisk.m
@@ -1,4 +1,4 @@
-function node=orthdisk(c0,c1,r,ndiv,v1)
+function node=orthdisk(c0,c1,r,ndiv,v1,angle0)
 %
 % node=orthdisk(c0,c1,r,ndiv)
 %
@@ -11,6 +11,11 @@ function node=orthdisk(c0,c1,r,ndiv,v1)
 %     c1: a 1x3 vector to define a direction vector c1-c0
 %     r: the radius of the disk that is orthogonal to c1-c0, passing through c0
 %     ndiv: division count to approximate a circle by a polygon, if ignored, ndiv=20
+%     v1: a 1x3 vector specifying the first point on the output 3D disk. if
+%         v1 is not perpendicular to c1-c0, the disk rotation axis is
+%         changed to cross(v1,cross(c1-c0,v1)); 
+%     angle0: angle0 represents the angle (in radian) of the 1st point in 
+%         the 3D disk if placed on the x-y plane.
 %
 % output:
 %     node: the 3D vertices of the disk
@@ -23,9 +28,13 @@ v0=c1-c0;
 
 if(nargin>=5)
     vt=cross(v0,v1);
-    if(norm(vt)>1e-5) % input is not orthogonal
+    if(abs(dot(v0(:),v1(:)))>1e-5) % input is not orthogonal
         v0=cross(v1,vt);
     end
+end
+
+if(nargin<6)
+    angle0=0;
 end
 
 if(nargin<=2)
@@ -36,7 +45,7 @@ if(nargin<=3)
 end
 
 dt=2*pi/ndiv;
-theta=dt:dt:2*pi;
+theta=angle0+dt:dt:2*pi+angle0;
 cx=r*cos(theta);
 cy=r*sin(theta);
 pp=[cx(:) cy(:) zeros(ndiv,1)];
