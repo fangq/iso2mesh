b1feced588d34fddf9d348fcafa433319ef70d8a
Qianqian Fang <fangqq@gmail.com>
2018-08-06 14:05:57 -0400

    revert mcpath to remove debug info

--- mcpath.m
+++ mcpath.m
@@ -21,13 +21,12 @@ function binname=mcpath(fname, ext)
 %
 % -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
 %
-which(mfilename)
 
 p=getvarfrom({'caller','base'},'ISO2MESH_BIN');
 binname=[];
 if(isempty(p))
 	% the bin folder under iso2mesh is searched first
-	tempname=[fileparts(which(mfilename)) filesep 'bin' filesep fname]
+	tempname=[fileparts(which(mfilename)) filesep 'bin' filesep fname];
 	if(exist([fileparts(which(mfilename)) filesep 'bin'])==7)
         if(nargin>=2)
             if(exist([tempname ext],'file'))
