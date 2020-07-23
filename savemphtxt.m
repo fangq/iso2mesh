aebd015228cb039ea9add0f2c87aeef24903cba9
unknown <weisheep@yeah.net>
2020-07-23 09:15:15 +0800

    remove for loop savemphtxt 3

--- savemphtxt.m
+++ savemphtxt.m
@@ -1,4 +1,5 @@
 function savemphtxt(node, face, elem, filename)
+%
 % savemphtxt(node, face, elem, filename)
 %
 % save tetrahedron mesh to comsol file (.mphtxt)
@@ -18,6 +19,9 @@ function savemphtxt(node, face, elem, filename)
 n_node = size(node,1);
 n_face = size(face,1);
 n_elem = size(elem,1);
+
+elem(:,1:4)=meshreorient(node(:,1:3),elem(:,1:4));
+
 if size(face,2)==3
     face(:,4)=1;
 end
@@ -38,38 +42,25 @@ fprintf(fp,'0 1\n1\n5 mesh1\n1\n3 obj\n\n');
 fprintf(fp,'0 0 1\n4 Mesh\n2\n3\n%d\n1\n',n_node);
 
 % Write Node information
-
 fprintf(fp,'%.16f %.16f %.16f\n',node(:,1:3).');
-
 fprintf(fp, '\n2\n\n3 tri\n');
 
 % Write Tri information
 fprintf(fp, '\n3\n');
 fprintf(fp, '%d\n\n',n_face);
-% for i = 1:n_face
 fprintf(fp,'%d %d %d\n',face(:,1:3).');
-% end
 fprintf(fp, '\n1\n0\n');
 fprintf(fp, '%d\n',n_face);
-
 fprintf(fp,'%d\n',face(:,4).');
-
 fprintf(fp, '\n%d\n',n_face);
 a = repmat([0 0], n_face, 1);
-fprintf(fp,'%d %d\n', a); % no need for transpose since it's all 0s
-% for i = 1:n_face
-%     fprintf(fp,'0 0\n');
-% end
+fprintf(fp,'%d %d\n', a); 
 
 % Write Tet information
 fprintf(fp, '\n\n3 tet\n4\n\n%d\n', n_elem);
-%for i = 1:n_elem
 fprintf(fp,'%d %d %d %d\n',elem(:,1:4).');
-%end    
 fprintf(fp, '\n4\n0\n%d\n%d\n', n_elem);
-% for i = 1:n_elem
 fprintf(fp,'%d\n',elem(:,5).');
-% end 
 fprintf(fp,'\n0\n');
 fclose(fp);
 
