8c35d0d707f5c01b86087d4a657efd3a02f4ff39
Qianqian Fang <fangqq@gmail.com>
2021-07-30 00:00:32 -0400

    can handle both 2D and 3D image in bwislands

--- bwislands.m
+++ bwislands.m
@@ -22,9 +22,10 @@ idx=find(1-img(:));
 islands={};
 
 count=1;
-while(length(idx))
-        [I,J]=ind2sub(size(img),idx(1));
-	imgnew=imfill(img,[I,J]);
+out=cell(1,ndims(img));
+while(~isempty(idx))
+    [out{:}]=ind2sub(size(img),idx(1));
+	imgnew=imfill(img,cell2mat(out));
 	islands{count}=find(imgnew~=img);
 	count=count+1;
 	img=imgnew;
