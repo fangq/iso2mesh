51c60f10f2efd8666b20cc0251b77f0ac099057d
Qianqian Fang <fangqq@gmail.com>
2025-02-23 13:35:01 -0500

    [feat] significantly speed up elemvolume using vectorization

--- elemvolume.m
+++ elemvolume.m
@@ -19,25 +19,22 @@ function vol = elemvolume(node, elem, option)
 % -- this function is part of iso2mesh toolbox (http://iso2mesh.sf.net)
 %
 
+v1 = node(elem(:, 1), :);
+v2 = node(elem(:, 2), :);
+v3 = node(elem(:, 3), :);
+
+edge1 = v2 - v1;
+edge2 = v3 - v1;
+
 if (size(elem, 2) == size(node, 2))
-    enum = size(elem, 1);
-    vol = zeros(enum, 1);
-    acol = ones(3, 1);
-    for i = 1:enum
-        e1 = det([node(elem(i, :), 2), node(elem(i, :), 3), acol]);
-        e2 = det([node(elem(i, :), 3), node(elem(i, :), 1), acol]);
-        e3 = det([node(elem(i, :), 1), node(elem(i, :), 2), acol]);
-        vol(i) = sqrt(e1 * e1 + e2 * e2 + e3 * e3) / 2;
-    end
+    vol = 0.5 * vecnorm(cross(edge1, edge2, 2), 2, 2);
     return
 end
-dim = size(elem, 2);
-enum = size(elem, 1);
-vol = zeros(enum, 1);
-for i = 1:enum
-    detmat = [node(elem(i, :), :)'; ones(1, dim)];
-    vol(i) = det(detmat);
-end
+
+v4 = node(elem(:, 4), :);
+edge3 = v4 - v1;
+vol = -dot(edge1, cross(edge2, edge3, 2), 2);
+
 if (nargin == 3 && strcmp(option, 'signed'))
     vol = vol / prod(1:size(node, 2));
 else
