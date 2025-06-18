743b87603ca93c9c9682d26f30b462df442caf54
Aiden Lewis <ayedyn@gmail.com>
2025-06-17 22:16:25 -0400

    [bug] fix #87, qmeshcut reshape error

--- qmeshcut.m
+++ qmeshcut.m
@@ -200,7 +200,8 @@ tripatch = reshape(tripatch(find(tripatch)), [3, length(tricut)])';
 % convhulln)
 
 quadpatch = emap(quadcut, :)';
-quadpatch = reshape(quadpatch(find(quadpatch)), [4, length(quadpatch)])';
+numquads = length(quadcut);
+quadpatch=reshape(quadpatch(find(quadpatch)),[4,numquads])';
 
 % combine the two sets to create the final facedata
 % using the matching-tetrahedra algorithm as shown in
